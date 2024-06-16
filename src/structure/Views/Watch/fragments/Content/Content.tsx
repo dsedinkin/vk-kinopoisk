d8c176d86ae49896fed43216108496ce16bae52a
dsedinkin <seadinkin2002@yandex.ru>
2024-06-16 23:22:58 +0500

    many updates, add logic and api

--- src/structure/Views/Watch/fragments/Content/Content.tsx
+++ src/structure/Views/Watch/fragments/Content/Content.tsx
@@ -1,17 +1,19 @@
-import {} from "react";
-import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
-import { classNames } from "engine/utils";
+import { useState, useEffect } from "react";
+import { useRouteNavigator, useParams } from "@vkontakte/vk-mini-apps-router";
+import api from "engine/api";
+import { classNames, parseResponseKinopoiskDocs } from "engine/utils";
 
 import {
   Button,
   ButtonGroup,
   Div,
+  Header,
+  Headline,
   MiniInfoCell,
   Subhead,
   Text,
-  Header,
 } from "@vkontakte/vkui";
-import { Poster } from "engine/components";
+import { ContentLoading, Poster } from "engine/components";
 
 import {
   Icon20ArticleOutline,
@@ -27,83 +29,130 @@ interface IContentProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Content: React.FC<IContentProps> = ({ className, ...restProps }) => {
   const routeNavigator = useRouteNavigator();
 
-  const poster = {
-    url: "https://image.openmoviedb.com/kinopoisk-images/1946459/bf93b465-1189-4155-9dd1-cb9fb5cb1bb5/orig",
-    previewUrl:
-      "https://image.openmoviedb.com/kinopoisk-images/1946459/bf93b465-1189-4155-9dd1-cb9fb5cb1bb5/x1000",
+  const params = useParams<"id">();
+
+  const [response, setResponse] = useState<any>({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const handleRefresh = () => {
+    setError(false);
+    setLoading(true);
+    if (typeof Number(params?.id) === "number") {
+      api
+        .getById(String(params?.id))
+        .then((resp) => {
+          setResponse(parseResponseKinopoiskDocs(resp) || {});
+        })
+        .catch((error) => {
+          console.log({ error });
+          setError(true);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      setError(true);
+      setLoading(false);
+    }
   };
-  const rating = {
-    kp: 8.826,
-    imdb: 8.5,
-    filmCritics: 6.8,
-    russianFilmCritics: 100,
-    await: null,
-  };
-  const name = "1+1";
-  const shortDescription =
-    "Аристократ на коляске нанимает в сиделки бывшего заключенного. Искрометная французская комедия с Омаром Си";
-  const genres = [
-    {
-      name: "драма",
-    },
-    {
-      name: "комедия",
-    },
-  ];
-  const year = 2011;
-  const movieLength = 112;
-  const description =
-    "Пострадав в результате несчастного случая, богатый аристократ Филипп нанимает в помощники человека, который менее всего подходит для этой работы, – молодого жителя предместья Дрисса, только что освободившегося из тюрьмы. Несмотря на то, что Филипп прикован к инвалидному креслу, Дриссу удается привнести в размеренную жизнь аристократа дух приключений.";
+
+  useEffect(() => {
+    handleRefresh();
+  }, []);
 
   return (
     <div {...restProps} className={classNames("Content Group", className)}>
-      <div className="Group__content">
-        <div className="WatchHeader">
-          <div className="WatchHeader__before">
-            <Poster
-              favorite={true}
-              rating={rating?.kp?.toFixed(1)}
-              src={poster?.url}
-            />
+      <ContentLoading loading={loading} error={error} onRefresh={handleRefresh}>
+        <div className="Group__content">
+          <div className="WatchHeader">
+            <div className="WatchHeader__before">
+              <Poster
+                favorite={true}
+                rating={response?.rating}
+                src={response?.src}
+              />
+            </div>
+            <div className="WatchHeader__after">
+              {response?.name || response?.id ? (
+                <Subhead
+                  Component="span"
+                  className="CustomCell__subhead CustomCell__text"
+                >
+                  {response?.name || response?.id}
+                </Subhead>
+              ) : (
+                <></>
+              )}
+              {response?.alternativeName ? (
+                <Headline
+                  Component="span"
+                  className="CustomCell__headline"
+                  weight="3"
+                >
+                  {response?.alternativeName}
+                </Headline>
+              ) : (
+                <></>
+              )}
+              {response?.shortDescription ? (
+                <MiniInfoCell before={<Icon20ArticleOutline />} textWrap="full">
+                  {response?.shortDescription}
+                </MiniInfoCell>
+              ) : (
+                <></>
+              )}
+              {response?.genresString ? (
+                <MiniInfoCell
+                  before={<Icon20ListBulletOutline />}
+                  textWrap="full"
+                >
+                  {response?.genresString}
+                </MiniInfoCell>
+              ) : (
+                <></>
+              )}
+              {response?.year ? (
+                <MiniInfoCell
+                  before={<Icon20CalendarOutline />}
+                  textWrap="full"
+                >
+                  {response?.year}
+                </MiniInfoCell>
+              ) : (
+                <></>
+              )}
+              {response?.movieLength ? (
+                <MiniInfoCell before={<Icon20RecentOutline />} textWrap="full">
+                  {response?.movieLength + " мин."}
+                </MiniInfoCell>
+              ) : (
+                <></>
+              )}
+              <Div>
+                <ButtonGroup mode="horizontal" stretched>
+                  <Button mode="primary" size="l">
+                    В избранное
+                  </Button>
+                  <Button mode="secondary" size="l">
+                    Скопировать ссылку
+                  </Button>
+                </ButtonGroup>
+              </Div>
+            </div>
           </div>
-          <div className="WatchHeader__after">
-            <Subhead
-              Component="span"
-              className="CustomCell__subhead CustomCell__text"
-            >
-              {name}
-            </Subhead>
-            <MiniInfoCell before={<Icon20ArticleOutline />}>
-              {shortDescription}
-            </MiniInfoCell>
-            <MiniInfoCell before={<Icon20ListBulletOutline />}>
-              {genres?.map((value) => value.name).join(", ")}
-            </MiniInfoCell>
-            <MiniInfoCell before={<Icon20CalendarOutline />}>
-              {year}
-            </MiniInfoCell>
-            <MiniInfoCell before={<Icon20RecentOutline />}>
-              {movieLength + " мин."}
-            </MiniInfoCell>
+          {response?.description ? (
             <Div>
-              <ButtonGroup mode="horizontal" stretched>
-                <Button mode="primary" size="l">
-                  В избранное
-                </Button>
-                <Button mode="secondary" size="l">
-                  Скопировать ссылку
-                </Button>
-              </ButtonGroup>
+              <Header mode="secondary" style={{ padding: 0 }}>
+                Описание
+              </Header>
+              <Text>{response?.description}</Text>
             </Div>
-          </div>
+          ) : (
+            <></>
+          )}
         </div>
-        <Div>
-          <Header mode="secondary" style={{ padding: 0 }}>
-            Описание
-          </Header>
-          <Text>{description}</Text>
-        </Div>
-      </div>
+      </ContentLoading>
     </div>
   );
 };
