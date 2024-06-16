377b320b85bcebd850d911ff74686b109040e893
dsedinkin <seadinkin2002@yandex.ru>
2024-06-16 22:17:56 +0500

    add api method search

--- src/engine/api/index.ts
+++ src/engine/api/index.ts
@@ -1,21 +1,36 @@
 import { getter, setter } from "elum-state/react";
 import { } from "engine/state";
 
-// import { } from "./methods";
-
 import { TRequest } from "engine/types";
 
 import { } from "engine/action";
 
-const request: TRequest = ({ }) =>
-  new Promise((resolve, reject) => {
+const options = {
+  method: "GET",
+  headers: { accept: "application/json", "X-API-KEY": "" }
+};
 
+const state = new Map();
+
+const request: TRequest = ({ path }) =>
+  new Promise((resolve, reject) => {
+    const dataString = JSON.stringify(path);
+    const json = state.get(dataString);
+    if (json) {
+      resolve(json);
+    } else {
+      fetch(`https://api.kinopoisk.dev/v1.4/${path}`, options)
+        .then((data) => data.json())
+        .then((resp) => {
+          state.set(dataString, resp);
+          resolve(resp);
+        })
+        .catch((error) => reject(error));
+    }
   });
 
-const api = (path: any) => {
-  return {
-
-  };
+const api = {
+  search: (params: string) => request({ path: `movie?${params}` })
 };
 
 export default api;
