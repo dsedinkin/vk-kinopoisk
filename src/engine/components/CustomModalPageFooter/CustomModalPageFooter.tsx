d7dada48ba613dc1ae792167258f5e0ea9575922
dsedinkin <seadinkin2002@yandex.ru>
2024-06-16 16:44:30 +0500

    to ease

--- src/engine/components/CustomModalPageFooter/CustomModalPageFooter.tsx
+++ src/engine/components/CustomModalPageFooter/CustomModalPageFooter.tsx
@@ -1,27 +1,13 @@
 import { classNames } from "engine/utils";
 
-import { Div, Button } from "@vkontakte/vkui";
+import { Div } from "@vkontakte/vkui";
 
 import "./CustomModalPageFooter.css";
 
-interface ICustomModalPageFooter extends React.HTMLAttributes<HTMLDivElement> {
-  disabled: boolean;
-  mode: string;
-  onAddClick: () => void;
-  onDeleteClick: () => void;
-  onEditClick: () => void;
-  textAddButton: string;
-  textEditButton: string;
-}
+interface ICustomModalPageFooter extends React.HTMLAttributes<HTMLDivElement> {}
 
 const CustomModalPageFooter: React.FC<ICustomModalPageFooter> = ({
-  disabled,
-  mode,
-  onAddClick,
-  onDeleteClick,
-  onEditClick,
-  textAddButton,
-  textEditButton,
+  children,
   className,
   ...restProps
 }) => {
@@ -30,27 +16,7 @@ const CustomModalPageFooter: React.FC<ICustomModalPageFooter> = ({
       {...restProps}
       className={classNames("CustomModalPageFooter", className)}
     >
-      {mode === "add" ? (
-        <Div className="CustomModalPageFooter__add">
-          <Button onClick={onAddClick} disabled={disabled}>
-            {textAddButton}
-          </Button>
-        </Div>
-      ) : (
-        <></>
-      )}
-      {mode === "edit" ? (
-        <Div className="CustomModalPageFooter__edit">
-          <Button onClick={onDeleteClick} appearance="negative" mode="link">
-            Удалить
-          </Button>
-          <Button onClick={onEditClick} disabled={disabled}>
-            {textEditButton}
-          </Button>
-        </Div>
-      ) : (
-        <></>
-      )}
+      <Div className="CustomModalPageFooter__content">{children}</Div>
     </div>
   );
 };
