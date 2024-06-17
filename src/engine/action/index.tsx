493334de8c610ccf91b3c4d671eea3d4cfa1bf61
dsedinkin <seadinkin2002@yandex.ru>
2024-06-18 01:35:24 +0500

    add actions copyText and favorites

--- src/engine/action/index.tsx
+++ src/engine/action/index.tsx
@@ -13,3 +13,54 @@ export const setSnackbar = (message: any) => {
     />
   );
 };
+
+export const copyText = (text: string) => {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      setSnackbar({
+        is_error: false,
+        text: "Скопировано",
+      });
+    })
+    .catch(() => {
+      setSnackbar({
+        is_error: true,
+        text: "Не удалось скопировать",
+      });
+    });
+};
+
+export const storage = {
+  get: (key: string) => localStorage?.getItem(key),
+  set: (key: string, value: string) => localStorage?.setItem(key, value),
+};
+
+export const getFavorites = () => storage.get("FAVORITES")?.split(",") || [];
+
+export const isFavorites = (id: string) => {
+  const idString = String(id);
+  const prevState = getFavorites();
+  const index = prevState?.indexOf(idString);
+  return index !== -1;
+};
+
+export const setFavorites = (id: string) => {
+  const idString = String(id);
+  const prevState = getFavorites();
+  const index = prevState?.indexOf(idString);
+  if (index !== -1) {
+    delete prevState[index];
+    setSnackbar({
+      is_error: false,
+      text: "Удалено из избранного",
+    });
+  } else {
+    prevState.push(idString);
+    setSnackbar({
+      is_error: false,
+      text: "Добавлено в избранное",
+    });
+  }
+  storage.set("FAVORITES", prevState?.join(","));
+};
